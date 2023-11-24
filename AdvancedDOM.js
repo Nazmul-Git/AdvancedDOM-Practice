b576db447dd50a68742a75cf812f58be7b7d744a
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-11-24 23:50:13 +0600

    Implement selecting dom element.

---
+++ AdvancedDOM.js
@@ -0,0 +1,8 @@
+// SELECTING ELEMENTS
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.querySelector('.div-1'));
+console.log(document.querySelectorAll('.section'));
+console.log(document.getElementById('p1'));
+console.log(document.getElementsByTagName('img'));
+console.log(document.getElementsByClassName('section'));
