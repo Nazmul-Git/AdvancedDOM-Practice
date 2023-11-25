83efa245640cd289be0407f50844daec4e244687
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-11-25 13:58:54 +0600

    Add a new element inline style.

--- AdvancedDOM.js
+++ AdvancedDOM.js
@@ -10,9 +10,19 @@ console.log(document.getElementsByClassName('section'));
 // creating and inserting elements
 const newElement=document.createElement('div');
 newElement.classList.add('message');
-newElement.innerHTML= 'Hello ! something happen when you click this <button>Got it!</button>';
+newElement.innerHTML= 'Hello ! something happen when you click this <button class="btn">Got it!</button>';
 const header=document.querySelector('.header');
 header.prepend(newElement);
 
 const footer=document.querySelector('.footer');
 // footer.append(newElement);
+
+// delete message
+document.querySelector('.btn').addEventListener('click', ()=>{
+    newElement.remove();
+})
+
+// inline style
+newElement.style.backgroundColor='green';
+newElement.style.width='120%';
+
