a9f15457b767bd60c4ef39fb46d0d6acab6a52a1
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-11-25 13:35:32 +0600

    ACreate a new element.

--- AdvancedDOM.js
+++ AdvancedDOM.js
@@ -6,3 +6,13 @@ console.log(document.querySelectorAll('.section'));
 console.log(document.getElementById('p1'));
 console.log(document.getElementsByTagName('img'));
 console.log(document.getElementsByClassName('section'));
+
+// creating and inserting elements
+const newElement=document.createElement('div');
+newElement.classList.add('message');
+newElement.innerHTML= 'Hello ! something happen when you click this <button>Got it!</button>';
+const header=document.querySelector('.header');
+header.prepend(newElement);
+
+const footer=document.querySelector('.footer');
+// footer.append(newElement);
